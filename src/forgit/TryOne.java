ad75d7ca2a0c3ac24f51a9ca1f1f7df4a21aa026
Shivam <shivamvats141@gmail.com>
2024-12-01 12:53:18 +0530

    firstinit

---
+++ src/forgit/TryOne.java
@@ -0,0 +1,16 @@
+package forgit;
+
+public class TryOne {
+
+	public TryOne() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("helooo");
+		// TODO Auto-generated method stub
+
+	}
+
+}
